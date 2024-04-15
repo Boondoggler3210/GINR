fead50ce0d360e9b45d245c8832527df961fe140
Boondoggler3210 <mattpage9@gmail.com>
2024-04-15 10:33:25 +0100

    Add project files.

---
+++ Movement.cs
@@ -0,0 +1,81 @@
+using System.Diagnostics.Contracts;
+
+namespace GINR;
+
+public class Movement
+{
+    public string Type { get; set; }
+    public decimal Quantity { get; set; }
+    public decimal ItemCost { get; set; }
+    public decimal StockQuantityBefore { get; set; }
+    public decimal StockQuantityAfter { get; set; }    
+    public decimal AverageCostBefore { get; set; }
+    public decimal AverageCostAfter { get; set; }
+    public decimal NominalValueBefore { get; set; }
+    public decimal NominalValueAfter { get; set; }
+    public decimal NominalAdjustment { get; set; }
+    public decimal NominalValueAfterAdjustment { get; set; }   
+    public decimal ExpectedNominalValue { get; set; }
+    public bool IsCorrect => NominalValueAfterAdjustment == ExpectedNominalValue;
+    public Movement()
+    {
+
+    }
+
+    public Movement(Movement previousMovement, decimal quantity, decimal itemCost)
+    {
+        Type = quantity > 0 ? "Receipt" : "Issue";
+        Quantity = quantity;
+        ItemCost = itemCost;
+        StockQuantityBefore = previousMovement.StockQuantityAfter;
+        StockQuantityAfter = previousMovement.StockQuantityAfter + quantity;
+        AverageCostBefore = previousMovement.AverageCostAfter;
+        
+        CalculateAverageCost();
+
+        NominalValueBefore = previousMovement.NominalValueAfterAdjustment;
+        NominalValueAfter = NominalValueBefore + (quantity * itemCost);
+
+        CalculateNominalAdjustment();
+
+        NominalValueAfterAdjustment = NominalValueAfter + NominalAdjustment;
+
+        ExpectedNominalValue = StockQuantityAfter * AverageCostAfter;
+
+    }
+
+    private decimal CalculateAverageCost()
+    {
+        if(StockQuantityBefore > 0)
+        {
+            if(StockQuantityAfter != 0)
+            {
+                AverageCostAfter = ((StockQuantityBefore * AverageCostBefore) + (Quantity * ItemCost)) / StockQuantityAfter;
+            }
+            else
+            {
+                AverageCostAfter = 0;
+            }
+
+        }
+        else
+        {
+            AverageCostAfter = ItemCost;
+        }
+
+        return AverageCostAfter;
+    }
+    private decimal CalculateNominalAdjustment()
+    {
+        if(StockQuantityBefore < 0)
+        {
+            NominalAdjustment = (StockQuantityBefore * AverageCostAfter) - (StockQuantityBefore * AverageCostBefore);
+        }
+        else
+        {
+            NominalAdjustment = 0;
+        }
+
+        return NominalAdjustment;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel;
+using GINR;
+
+Console.WriteLine("Hello, World!");
+
+List<Movement> movements = new List<Movement>();
+
+Console.WriteLine("Add a movement in the format \"quantity, item cost\" where quantity is a positive number for a receipt and a negative number for an issue.");
+Console.WriteLine("Example: 5, 100");
+while(true)
+{
+    Console.WriteLine("Add a movement:");
+    var line = Console.ReadLine();
+    if(line == "exit")
+    {
+        break;
+    }
+    var arguments = line.Split(',');
+    var quantity = decimal.Parse(arguments[0]);
+    var itemCost = decimal.Parse(arguments[1]);
+
+    movements.Add(new Movement(movements.Count > 0 ? movements.Last() :new Movement(), quantity, itemCost));
+    
+    Table table = new Table("Type", "Quantity", "Cost", "StkBefore", "StockAfter", "AveCost Before", "AveCost After", "NomValue Before", "NomValue After", "NomAdjustment", "NomValueAfAdj", "Expected NomValue", "IsCorrect?");
+    
+    foreach (var movement in movements)
+    {
+       table.AddRow(movement.Type, movement.Quantity, movement.ItemCost, movement.StockQuantityBefore, movement.StockQuantityAfter, movement.AverageCostBefore, movement.AverageCostAfter, movement.NominalValueBefore, movement.NominalValueAfter, movement.NominalAdjustment, movement.NominalValueAfterAdjustment, movement.ExpectedNominalValue, movement.IsCorrect);
+    }
+
+    table.Print();
+}
+
+
